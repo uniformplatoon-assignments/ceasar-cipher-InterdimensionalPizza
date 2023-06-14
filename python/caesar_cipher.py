919ceaf1a7d41d9d1afe14436f4c4d73fc7de20b
interdimensionalpizza <michaellake05@gmail.com>
2023-06-14 16:18:00 -0700

    finished challenge in python

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,11 @@
 def caesar_cipher(string, shift_amount):
+    abc = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    newstr = ''
+    for char in string:
+        if char in abc:
+            #newstr is appended with the second occurrence of the letter in abc that is matched, and then shifted.
+            #This is done to make sure that once the letter is shifted, it still selects a letter of the same case.
+            newstr += abc[abc.index(char, abc.index(char)+1) + shift_amount]
+        else:
+            newstr += char
+    return newstr
